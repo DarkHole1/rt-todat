6adbd564cd167a9a55e47f3944121d2685942678
DarkHole1 <dark.hole1@yandex.ru>
2020-11-29 13:17:31 +0300

    Added base template

--- zpg.js
+++ zpg.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('madeby').innerText = madeby();
 
+  var handler = null;
+
   var content = document.getElementById('content');
   var header = document.getElementById('header');
-  document.getElementById('btn').addEventListener('click', function() {
-    this.innerText = 'Попробовать снова';
-    content.innerText = _.zip(randomUniqueNames(10), randomUniqueStatuses(10)).map(function(e) {
-      var gender = randomGender();
-      return e[0][0][gender] + ' ' + e[0][1][gender] + ' — ' + e[1][gender];
-    }).join('\n');
-    header.innerText = 'RT-' + _.random(1000, 9999);
+  var btn = document.getElementById('btn');
+  btn.addEventListener('click', function() {
+    try {
+      handler();
+    } catch (e) { }
   })
 })
